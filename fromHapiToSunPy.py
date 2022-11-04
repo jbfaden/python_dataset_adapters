0c70b2c0afefd90b48d9e43da39434908d1cdc7f
Jeremy Faden <faden@cottagesystems.com>
2022-11-04 08:02:53 -0500

    tests hadn't hit case where units table was needed

--- fromHapiToSunPy.py
+++ fromHapiToSunPy.py
@@ -1,9 +1,119 @@
+import pandas as pd
 import astropy.units as u
 from sunpy.timeseries import GenericTimeSeries
-import hapiclient
+from sunpy.util.exceptions import warn_user
 from hapiclient.hapitime import hapitime2datetime
-import pandas as pd
 
+# This was copied from https://github.com/sunpy/sunpy/blob/main/sunpy/io/cdf.py, which
+# contains a _known_units dictionary.  It is reused here, since the same
+# units will appear in the CDAWeb HAPI server.  --Jeremy Faden
+#
+# Unfortunately (unlike e.g. FITS), there is no standard for the strings that
+# CDF files use to represent units. To allow for this we maintain a dictionary
+# mapping unit strings to their astropy unit equivalents.
+#
+# Please only add new entries if
+#   1. A user identifies which specific mission/data source they are needed for
+#   2. The mapping from the string to unit is un-ambiguous. If we get this
+#      wrong then users will silently have the wrong units in their data!
+_known_units = {'ratio': u.dimensionless_unscaled,
+                'NOTEXIST': u.dimensionless_unscaled,
+                'Unitless': u.dimensionless_unscaled,
+                'unitless': u.dimensionless_unscaled,
+                'Quality_Flag': u.dimensionless_unscaled,
+                'None': u.dimensionless_unscaled,
+                'none': u.dimensionless_unscaled,
+                ' none': u.dimensionless_unscaled,
+                'counts': u.dimensionless_unscaled,
+                'cnts': u.dimensionless_unscaled,
+
+                'microW m^-2': u.mW * u.m**-2,
+
+                'years': u.yr,
+                'days': u.d,
+
+                '#/cc': u.cm**-3,
+                '#/cm^3': u.cm**-3,
+                'cm^{-3}': u.cm**-3,
+                'particles cm^-3': u.cm**-3,
+                'n/cc (from moments)': u.cm**-3,
+                'n/cc (from fits)': u.cm**-3,
+                'Per cc': u.cm**-3,
+                '#/cm3': u.cm**-3,
+                'n/cc': u.cm**-3,
+
+                'km/sec': u.km / u.s,
+                'km/sec (from fits)': u.km / u.s,
+                'km/sec (from moments)': u.km / u.s,
+                'Km/s': u.km / u.s,
+
+                'Volts': u.V,
+
+                'earth radii': u.earthRad,
+                'Re': u.earthRad,
+                'Earth Radii': u.earthRad,
+                'Re (1min)': u.earthRad,
+                'Re (1hr)': u.earthRad,
+
+                'Degrees': u.deg,
+                'degrees': u.deg,
+                'Deg': u.deg,
+                'deg (from fits)': u.deg,
+                'deg (from moments)': u.deg,
+                'deg (>200)': u.deg,
+
+                'Deg K': u.K,
+                'deg_K': u.K,
+                '#/{cc*(cm/s)^3}': (u.cm**3 * (u.cm / u.s)**3)**-1,
+                'sec': u.s,
+                'Samples/s': 1 / u.s,
+
+                'seconds': u.s,
+                'nT GSE': u.nT,
+                'nT GSM': u.nT,
+                'nT DSL': u.nT,
+                'nT SSL': u.nT,
+                'nT (1min)': u.nT,
+                'nT (3sec)': u.nT,
+                'nT (1hr)': u.nT,
+                'nT (>200)': u.nT,
+
+                'msec': u.ms,
+                'milliseconds': u.ms,
+
+                '#/cm2-ster-eV-sec': 1 / (u.cm**2 * u.sr * u.eV * u.s),
+                '#/(cm^2*s*sr*MeV/nuc)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '#/(cm^2*s*sr*Mev/nuc)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '#/(cm^2*s*sr*Mev/nucleon)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '#/(cm2-steradian-second-MeV/nucleon) ': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '1/(cm2 Sr sec MeV/nucleon)': 1 / (u.cm**2 * u.sr * u.s * u.MeV),
+                '1/(cm**2-s-sr-MeV)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '1/(cm**2-s-sr-MeV/nuc.)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '(cm^2 s sr MeV/n)^-1': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                'cm!E-2!Nsr!E-1!Nsec!E-1!N(MeV/nuc)!E-1!N': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                'cm!E-2!Nsr!E-1!Nsec!E-1!NMeV!E-1!N': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '1/(cm^2 sec ster MeV)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                '(cm^2 s sr MeV)^-1': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                'cnts/sec/sr/cm^2/MeV': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+
+                'particles / (s cm^2 sr MeV)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                'particles / (s cm^2 sr MeV/n)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+                'particles/(s cm2 sr MeV/n)': 1 / (u.cm**2 * u.s * u.sr * u.MeV),
+
+                '1/(cm**2-s-sr)': 1 / (u.cm**2 * u.s * u.sr),
+                '1/(SQcm-ster-s)': 1 / (u.cm**2 * u.s * u.sr),
+                '1/(SQcm-ster-s)..': 1 / (u.cm**2 * u.s * u.sr),
+
+                'photons cm^-2 s^-1': 1 / (u.cm**2 * u.s),
+
+                'Counts/256sec': 1 / (256 * u.s),
+                'Counts/hour': 1 / u.hr,
+                'counts/min': 1 / u.min,
+                'counts / s': 1/u.s,
+                'counts/s': 1/u.s,
+                'cnts/sec': 1/u.s,
+                'counts s!E-1!N': 1/u.s,
+                }
 
 def hapi_to_time_series(hapidata):
     hdata, meta = hapidata
